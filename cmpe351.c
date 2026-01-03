0fbb3d49d6fb0be976026a4aff123b9b6a07b1d3
root <root@Lolo.localdomain>
2026-01-03 13:27:24 +0200

    Temporary debug version: input parsing and linked list verified

--- cmpe351.c
+++ cmpe351.c
@@ -1,7 +1,6 @@
  #include <stdio.h>
  #include <stdlib.h>
 
-/* ===== STRUCT DEFINITIONS ===== */
 typedef struct Process {
     int burst_time;        // CPU execution time
     int priority;          // Priority level
@@ -11,31 +10,81 @@ typedef struct Process {
     struct Process *next;  // Pointer to next process (linked list)
 } Process;
 
-/* ===== FUNCTION PROTOTYPES (optional for now) ===== */
 
 int main(int argc, char *argv[])
 {
+
+int burst, priority, arrival, queue;
+
+Process *head = NULL;
+Process *tail = NULL;
+
 FILE *inputFile;
 FILE *outputFile;
- //check if correct number of arguments
+
+//check if correct number of arguments
 if (argc != 3)
 {
    printf("Usage: %s input_file.txt output_file.txt\n",
 argv[0]);
    return 1;
 }
+
 //open input file (argv[1]) and output file (argv[2])
 inputFile = fopen(argv[1], "r");
 outputFile = fopen(argv[2], "w");
 
-if (inputFile == NULL || outputFile == NULL) {
+if (inputFile == NULL || outputFile == NULL) 
+{
     printf("Error opening file\n");
     return 1;
 }
-  //process data
- fclose(inputFile);
- fclose(outputFile);
 
-  //close files
+//process data
+while (fscanf(inputFile, "%d:%d:%d:%d",&burst, &priority, &arrival, &queue) == 4) 
+{
+
+    Process *newProcess = (Process *)malloc(sizeof(Process));
+      if (newProcess == NULL)
+   {
+       printf("Memory allocation failed\n");
+       return 1;
+   }
+
+  newProcess->burst_time = burst;
+  newProcess->priority = priority;
+  newProcess->arrival_time = arrival;
+  newProcess->queue_id = queue;
+  newProcess->waiting_time = 0;
+  newProcess->next = NULL;
+
+      if (head == NULL)
+   {
+        head = newProcess;
+        tail = newProcess;
+   }
+
+    else
+   {
+        tail->next = newProcess;
+        tail = newProcess;
+   }
+
+}
+//testing for correctness
+Process *temp = head;
+while (temp != NULL) {
+    printf("BT=%d P=%d AT=%d Q=%d\n",
+           temp->burst_time,
+           temp->priority,
+           temp->arrival_time,
+           temp->queue_id);
+    temp = temp->next;
+}
+
+//close files
+fclose (inputFile);
+fclose (outputFile);
+
   return 0;
 }
